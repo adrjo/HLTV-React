a71942a5e1a57921e608f4801de93a47aeb71fdd
adrjo <adrianjohansson16@gmail.com>
2025-09-11 16:38:14 +0200

    Add new post form

---
+++ src/components/NewPostForm.jsx
@@ -0,0 +1,81 @@
+import { useState } from "react"
+import "../styles/NewPostForm.css"
+import { Button, Input, Textarea } from "@headlessui/react";
+
+
+export function NewPostForm({ setShow }) {
+    const toggleShow = () => (
+        setShow(false)
+    )
+    const [title, setTitle] = useState("");
+    const [author, setAuthor] = useState("");
+    const [image, setImage] = useState("");
+    const [imageText, setImageText] = useState("");
+    const [content, setContent] = useState("");
+
+
+    const submit = () => {
+        console.log(title);
+        console.log(author);
+        console.log(image);
+        console.log(imageText);
+        console.log(content);
+
+    }
+
+    return (
+        <>
+            <div id="darken-bg" onClick={toggleShow} className="h-full w-full bg-[rgba(0,0,0,0.8)] fixed"></div>
+            <div className="formed">
+                <h2>New Post</h2>
+                <Input
+                    name="title"
+                    type="text"
+                    placeholder="Title"
+                    value={title}
+                    onChange={(e) => setTitle(e.target.value)}
+                />
+                <Input
+                    name="author"
+                    type="text"
+                    placeholder="Author"
+                    value={author}
+                    onChange={(e) => setAuthor(e.target.value)}
+                />
+                <Input
+                    name="image"
+                    type="text"
+                    placeholder="Header Image Link"
+                    value={image}
+                    onChange={(e) => setImage(e.target.value)}
+                />
+                <Input
+                    name="image_text"
+                    type="text"
+                    placeholder="Image text"
+                    value={imageText}
+                    onChange={(e) => setImageText(e.target.value)}
+                />
+
+                <Textarea
+                    name="content"
+                    value={content}
+                    onChange={(e) => setContent(e.target.value)}>
+                </Textarea>
+
+                <Button
+                    onClick={toggleShow}
+                    type="submit"
+                    className="bg-red-400 text-black">
+                    Cancel
+                </Button>
+                <Button
+                    onClick={submit}
+                    type="submit"
+                    className="bg-green-400 text-black">
+                    Submit
+                </Button>
+            </div>
+        </>
+    )
+}
--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -3,6 +3,9 @@ import { Header } from "../components/Header";
 import { Navbar } from "../components/Navbar";
 import "../styles/MainPage.css"
 import { NewsList } from "../components/NewsList";
+import { Button } from "@headlessui/react";
+import { NewPostForm } from "../components/NewPostForm";
+import { adminStore } from "../App";
 
 const featuredArticle = {
   title: "Spirit win Blast Open London 2025",
@@ -13,15 +16,39 @@ const featuredArticle = {
 export function MainPage() {
   //TODO: select random featured article on reload?
   const [article, setFeaturedArticle] = useState(featuredArticle);
+  const adminMode = adminStore((state) => state.adminModeToggled);
+
+  const news = [{
+    id: 0,
+    author: "Testauthor",
+    title: "Test",
+    img: "https://img-cdn.hltv.org/gallerypicture/9-jXDp3QMajFwZ0YckD_qe.jpg?auto=compress&ixlib=java-2.1.0&q=75&s=a715c020af2fedf5dc2517c050b5b66f",
+    imgText: "Test description",
+    content: "Test content\nTest content line 2"
+  }]
+
+  const [show, setShow] = useState(false);
+
+  const toggleShow = () => (
+    setShow(!show)
+  )
 
 
   return (
     <>
+      {show && <NewPostForm setShow={setShow}/>}
       <Header article={article} />
       <Navbar />
 
-      
-      <NewsList news={news}/>
+      {adminMode &&
+        <Button
+          onClick={toggleShow}
+          className="bg-green-400 m-5 p-3 text-black">
+          New Post
+        </Button>
+      }
+
+      <NewsList news={news} />
     </>
   )
 }
