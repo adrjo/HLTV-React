1a9c20f7a0e2e98aac035e56a7d3bcf7d7a8aca9
adrjo <adrianjohansson16@gmail.com>
2025-09-15 21:40:12 +0200

    Divide posts by day on the main page

--- src/components/NewsList.jsx
+++ src/components/NewsList.jsx
@@ -1,19 +1,45 @@
 import { adminStore } from "../App";
-import "../styles/NewsList.css"
-import { NewsEntry } from "./NewsEntry"
-
+import { NewsEntry } from "./NewsEntry";
 
 export function NewsList({ news }) {
     const adminMode = adminStore((state) => state.adminModeToggled);
 
+    let currentDay = -1;
+
     return (
         <main className="news mr-1 mb-5 flex flex-col">
             {news
-            .sort((a, b) => b.date - a.date) // newest posts first
-            .map((newsEntry) => (
-                <NewsEntry key={newsEntry.id} entry={newsEntry} editable={adminMode} />
-            ))}
+                .sort((a, b) => b.date - a.date) // newest posts first
+                .map((newsEntry) => {
+                    const postDate = new Date(newsEntry.date);
+                    const day = postDate.getDate();
 
+                    const elements = [];
+
+                    if (day != currentDay) {
+                        currentDay = day;
+                        const formatter = new Intl.DateTimeFormat("en-US", {
+                            month: "long",
+                            day: "numeric",
+                            year: "numeric",
+                        });
+
+                        elements.push(
+                            <h3 className="mt-4 mb-2 font-bold text-xl">
+                                Counter-Strike news on {formatter.format(postDate)}:
+                            </h3>
+                        );
+                    }
+
+                    elements.push(
+                        <NewsEntry
+                            entry={newsEntry}
+                            editable={adminMode}
+                        />
+                    );
+
+                    return elements;
+                })}
         </main>
-    )
+    );
 }
